3031e1295f479e999d019afdbdae371b2dff3e76
gadrian99 <adriangarcia1999@yahoo.com>
2020-07-12 04:52:53 -0400

    setup for multiple players to join

--- client/js/game.js
+++ client/js/game.js
@@ -15,14 +15,60 @@ var config = {
       create: create,
       update: update
     }
-  };
+};
 
-  var game = new Phaser.Game(config);
+var game = new Phaser.Game(config);
 
-  function preload() {}
+function preload() {
+  this.load.image('otherPlayer', 'assets/adventurer-idle-02.png');
+}
 
-  function create() {
+function create() {
+    var self = this;
     this.socket = io();
+    this.otherPlayers = this.physics.add.group();
+    this.socket.on('currentPlayers', function (players) {
+      Object.keys(players).forEach(function (id) {
+        if (players[id].playerId === self.socket.id) {
+          addPlayer(self, players[id]);
+        } else {
+          addOtherPlayers(self, players[id]);
+        }
+      });
+    });
+    this.socket.on('newPlayer', function (playerInfo) {
+      addOtherPlayers(self, playerInfo);
+    });
+    this.socket.on('disconnect', function (playerId) {
+      self.otherPlayers.getChildren().forEach(function (otherPlayer) {
+        if (playerId === otherPlayer.playerId) {
+          otherPlayer.destroy();
+        }
+      });
+    });
   }
 
-  function update() {}
+function update() {}
+
+function addPlayer(self, playerInfo) {
+  self.player = self.physics.add.image(playerInfo.x, playerInfo.y, 'player').setOrigin(0.5, 0.5).setDisplaySize(80, 70);
+  if (playerInfo.team === 'red') {
+    self.player.setTint(0x0000ff);
+  } else {
+    self.player.setTint(0xff0000);
+  }
+  self.player.setDrag(100);
+  self.player.setAngularDrag(100);
+  self.player.setMaxVelocity(200);
+}
+
+function addOtherPlayers(self, playerInfo) {
+  const otherPlayer = self.add.sprite(playerInfo.x, playerInfo.y, 'otherPlayer').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  if (playerInfo.team === 'blue') {
+    otherPlayer.setTint(0x0000ff);
+  } else {
+    otherPlayer.setTint(0xff0000);
+  }
+  otherPlayer.playerId = playerInfo.playerId;
+  self.otherPlayers.add(otherPlayer);
+}
